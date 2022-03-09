8e65b1477024269445a588579555e18b06ec88cd
moonelysian <dandyblack@naver.com>
2022-03-09 12:53:54 +0900

    add: day18

---
+++ yeji/DataStructure/LinkedList/merge-two-sorted-lists.py
@@ -0,0 +1,19 @@
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        head = ListNode(-1)
+        cursor = head
+        
+        while list1 != None and list2 != None:
+            if list1.val <= list2.val:
+                cursor.next = list1
+                list1 = list1.next
+            else:
+                cursor.next = list2
+                list2 = list2.next
+            cursor = cursor.next
+            
+        if list1 != None:
+            cursor.next = list1
+        else:
+            cursor.next = list2
+        return head.next
